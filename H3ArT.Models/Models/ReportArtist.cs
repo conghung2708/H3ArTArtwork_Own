6d3db6b46db04e30102bdf5c5c7ffd2f77528777
conghung2708 <159764304+conghung2708@users.noreply.github.com>
2024-03-03 21:52:43 +0700

    Add files via upload

---
+++ H3ArT.Models/Models/ReportArtist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H3ArT.Models.Models
+{
+    public class ReportArtist
+    {
+        [Key]
+        public int reportArtistID { get; set; }
+        public string artistID { get; set; }
+        public string reporterUserID { get; set; }
+    }
+}
---
+++ H3ArT.Models/Models/ReportArtwork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H3ArT.Models.Models
+{
+    public class ReportArtwork
+    {
+        [Key]
+        public int reportArtworkID { get; set; }
+        public int artworkID { get; set; }
+        public string reporterUserID { get; set; }
+    }
+}
